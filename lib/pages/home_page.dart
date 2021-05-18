5ade6a95576870ae1514d5933bf99a6e4e009513
Dhruvil <gosaliadhruvil70@gmail.com>
2021-05-19 01:41:09 +0530

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,27 @@ import 'package:ecom_catalog/models/catalog.dart';
 import 'package:ecom_catalog/widgets/drawer.dart';
 import 'package:ecom_catalog/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummylist = List.generate(10, (index) => CatalogModel.items[0]);
